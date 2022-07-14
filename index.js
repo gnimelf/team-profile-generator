7b3485e94211744d4262330329627d43e272f1be
justin <fezzygo@gmail.com>
2022-07-14 18:12:15 -0400

    added project structure

---
+++ index.js
@@ -0,0 +1,18 @@
+// import inquirer
+
+var team = [];
+
+
+// prompted to enter the team manager’s name
+
+// prompted to enter the team manager’s employee ID
+
+// prompted to enter the team manager’s email address
+
+// prompted to enter the team manager’s office number
+
+// presented with a menu with the option to add an engineer or an intern or to finish building my team 
+
+
+
+// Build team 
