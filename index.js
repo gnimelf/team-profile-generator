8629e0df403eae5ce1beb088f29e940725be401e
justin <fezzygo@gmail.com>
2022-07-15 23:36:54 -0400

    added prompts (index.js:1-105), add methods to each of the classes (Employee.js:5-21, Engineer.js:3-10, Intern.js:4,5,8-15), added class definition (Manager.js:1-13)

--- index.js
+++ index.js
@@ -1,18 +1,104 @@
-// import inquirer
+const inquirer = require("inquirer");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 
-var team = [];
+var team = []; // employee objects
 
+let teamBuild = false;
+let employeeType = 'manager';
 
-// prompted to enter the team manager’s name
+while (teamBuild === false) {
 
-// prompted to enter the team manager’s employee ID
+  // Get Manager Info
+  if (employeeType === "manager"){
+      inquirer.prompt([
+    // prompted to enter the team manager’s name
+    {
+      type:'input',
+      message: "Enter the manager's name",
+      name: managerName
+    },
+    // prompted to enter the team employee ID
+    {
+      type:'input',
+      message: "Enter the manager's employee ID",
+      name: managerID
+    },
+    // prompted to enter the team manager’s email address
+    {
+      type:'input',
+      message: 'Enter the Manager name',
+      name: managerEmail
+    },
+    // prompted to enter the team manager’s office number
+    {
+      type:'input',
+      message: 'Manager name',
+      name: managerName
+    }
+    // presented with a menu with the option to add an engineer or an intern or to finish building my team
+  ]);
+  }
+  else if (employeeType === "engineer") {
+    inquirer.prompt([
+      // prompted to enter the team manager’s name
+      {
+        type:'input',
+        message: "Enter the engineer's name",
+        name: managerName
+      },
+      // prompted to enter the team employee ID
+      {
+        type:'input',
+        message: "Enter the engineer's employee ID",
+        name: managerID
+      },
+      // prompted to enter the team manager’s email address
+      {
+        type:'input',
+        message: "Enter the engineer's email address",
+        name: managerEmail
+      },
+      // prompted to enter the team manager’s office number
+      {
+        type:'input',
+        message: "Enter the engineer's github's username",
+        name: engineerUsername
+      }
+      // presented with a menu with the option to add an engineer or an intern or to finish building my team
+    ]);
+  }
+  else if (employeeType === "intern"){
+    inquirer.prompt([
+      // prompted to enter the team manager’s name
+      {
+        type:'input',
+        message: "Enter the engineer's name",
+        name: managerName
+      },
+      // prompted to enter the team employee ID
+      {
+        type:'input',
+        message: "Enter the engineer's employee ID",
+        name: managerID
+      },
+      // prompted to enter the team manager’s email address
+      {
+        type:'input',
+        message: "Enter the engineer's email address",
+        name: managerEmail
+      },
+      // prompted to enter the team manager’s office number
+      {
+        type:'input',
+        message: "Enter the engineer's github's username",
+        name: engineerUsername
+      }
+      // presented with a menu with the option to add an engineer or an intern or to finish building my team
+    ]);
+  }
+}
 
-// prompted to enter the team manager’s email address
-
-// prompted to enter the team manager’s office number
-
-// presented with a menu with the option to add an engineer or an intern or to finish building my team 
-
-
-
-// Build team 
+// Build team
