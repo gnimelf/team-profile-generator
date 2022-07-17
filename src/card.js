5f60c000f484a2cc776b77dd825a1e8aa95acec6
justin <fezzygo@gmail.com>
2022-07-17 01:51:02 -0400

    added html card code (card.js:1-14)

---
+++ src/card.js
@@ -0,0 +1,14 @@
+card = `
+<div class="card border-light mb-3" style="max-width: 18rem;">
+    <div class="card-header">
+        <h3>Jared</h3>
+        <h4>Manager</h4>
+    </div>
+    <div class="card-body">
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">ID: 4</li>
+        <li class="list-group-item">Email: test@test.com</li>
+        <li class="list-group-item">Github: githubname</li>
+    </ul>
+    </div>
+</div>`;
